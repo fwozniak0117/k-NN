0b93ab17bab6d2daf8a9067d179ca770d38b6c8a
Filip Wozniak <s22703@pjwstk.edu.pl>
2022-03-21 00:00:53 +0100

    Observations from Test.set added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Observation> obs = readAndCreateObs(true,"Test-set.txt");
+
+            System.out.println(obs.get(6).getListOfParamets().get(2));
 
 
 
+    }
+
+    static ArrayList<Observation> readAndCreateObs(boolean isTest , String fileName) throws FileNotFoundException {
+        ArrayList <Observation> observations = new ArrayList<>();
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+        int numberOfArgs;
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] words = line.split(",");
+            numberOfArgs = words.length - 2;
+            double [] args = new double[numberOfArgs];
+            for (int i = 0; i < numberOfArgs ; i++) {
+                args[i] = Double.parseDouble(words[i]);
+            }
+            Observation observation = new Observation(isTest, words[words.length -1],args);
+            observations.add(observation);
+
+        }
+        return observations;
     }
 }
---
+++ src/Observation.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Observation extends ArrayList<Double> {
+    public static int lp;
+    private String name;
+    private boolean isTestSet;
+    private ArrayList <Double> listOfParamets = new ArrayList();
+
+    public Observation(boolean isTestSet, String name, double ... x) {
+        this.name = name;
+        this.isTestSet = isTestSet;
+        for (int i = 0; i <x.length; i++) {
+            listOfParamets.add(x[i]);
+        }
+        lp++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Double> getListOfParamets() {
+        return listOfParamets;
+    }
+
+    public String toString(){
+        ArrayList <Double> toPrint = getListOfParamets();
+        for (int i = 0; i < toPrint.size(); i++) {
+            System.out.print(toPrint.get(i));
+        }
+        return "";
+    }
+
+
+    }
+
